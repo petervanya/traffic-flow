0f4f80efb1fa3afbd504354d2b4e8fb56508c031
petervanya <peter.vanya@gmail.com>
2020-07-30 17:05:34 +0200

    Polish; consistency renaming; fixing broken repeated assignment of flows

--- mtsim/mtm.py
+++ mtsim/mtm.py
@@ -4,30 +4,34 @@ from numpy import sqrt, exp
 import networkx as nx
 
 
-class MTM():
+class MTM:
     """Macroscopic transport modelling class"""
     # global varialbles
     assignment_kinds = ["incremental"]
     basic_skim_kinds = ["t0", "tcur", "l"]
     
-    def __init__(self, v_intra=40.0):
+    def __init__(self, v_intra=40.0, verbose=False):
         """
+        Inputs
+        ======
         - v_intra : intrazonal speed in kmh
+        - verbose : level of printing information
         """
         self.G = nx.Graph() # the graph structure
         self.skims = {} # skim matrices
         self.dmats = {} # demand matrices
-        self.dstrat = pd.DataFrame(columns=["prod", "attr", "coeff"]) # demand strata
+        self.dstrat = pd.DataFrame(columns=["prod", "attr", "param"]) # demand strata
         self.dpar = pd.DataFrame(columns=["skim", "func", "param"]) # distribution parameters
         
         # ad-hoc data
         self.v_intra = v_intra
+        self.verbose = verbose
         
         
     def read_data(self, nodes, link_types, links):
         """
-        Input
-        =====
+        Inputs
+        ======
         - Nodes : dataframe [is_zone, name, coords, pop], index: id
         - Link types : dataframe [name, v0, qmax, a, b], index: id
         - Links : dataframe [id, id_lt, name, l, count], index: id_node_pair
@@ -93,7 +97,7 @@ class MTM():
         # sort node order
         
         # assign empty attributes
-        self.df_links["t0"] = self.df_links["l"] / self.df_links["v0"]
+        self.df_links["t0"] = self.df_links["l"] / self.df_links["v0"] * 60.0
         self.df_links["q"] = 0.0
         self.df_links["tcur"] = self.df_links["t0"]
         self.df_links["vcur"] = self.df_links["v0"]
@@ -104,10 +108,16 @@ class MTM():
         
     
     def compute_tcur_links(self):
-        """Compute current travel time wrt the intensities"""
+        """Compute current travel time and speed wrt the flows
+        and assign to the graph G"""
         self.df_links["tcur"] = self.df_links.apply(lambda x: \
             x["t0"] * (1.0 + x["a"] * (x["q"]/x["qmax"])**x["b"]), 1)
-        self.df_links["vcur"] = self.df_links["l"] / self.df_links["tcur"]
+        
+        self.df_links["vcur"] = self.df_links["l"] / self.df_links["tcur"] * 60.0
+        
+        # assign to the graph
+        nx.set_edge_attributes(self.G, self.df_links["tcur"], "tcur")
+        nx.set_edge_attributes(self.G, self.df_links["vcur"], "vcur")
         
 
     def _fill_graph(self):
@@ -122,16 +132,16 @@ class MTM():
     # =====
     # Trip generation
     # =====
-    def generate(self, name, prod, attr, coeff):
+    def generate(self, name, prod, attr, param):
         """
         Generate the key-value pairs of node columns.
         Apply the method separately for each demand stratum.
         
-        Input
-        =====
+        Inputs
+        ======
         - prod : productivity column
         - attr : attractivity column
-        - coeff : coefficient with which to multiply the productivity
+        - param : parameter with which to multiply the productivity
             (mean number of trips per day, fraction of the population)
         """
         assert prod in self.df_nodes.columns, \
@@ -139,7 +149,7 @@ class MTM():
         assert attr in self.df_nodes.columns, \
             "Attraction attribute not found in node columns."
         
-        self.dstrat.loc[name] = [prod, attr, coeff]
+        self.dstrat.loc[name] = [prod, attr, param]
     
     # =====
     # Skim matrices
@@ -155,33 +165,43 @@ class MTM():
         self._compute_skim_basic("l", **kw)
         self._compute_skim_basic("t0", **kw)
         self._compute_skim_basic("tcur", **kw)
-        
+              
             
     def _compute_skim_basic(self, kind, diagonal="density", density=1000.0):
+        """
+        General method to compute skim matrices from basic quantities
+        (free flow time, current time or distance).
+        
+        Inputs
+        ======
+        - kind : eg t0, tcur, l
+        - diagonal : way to compute the matrix diagonal
+        - density : average density per zone
+        """
         assert kind in self.basic_skim_kinds, \
             "Choose kind among %s." % self.basic_skim_kinds
         
-        dists = dict(nx.all_pairs_dijkstra(self.G, weight=kind))    
+        paths = dict(nx.all_pairs_dijkstra_path_length(self.G, weight=kind))
         self.skims[kind] = \
             pd.DataFrame(columns=self.df_zones.index,\
                          index=self.df_zones.index, dtype=float)
 
+        # REMOVE for loop for efficiency via df.loc[]
         for i in self.df_zones.index:
             for j in self.df_zones.index:
-                self.skims[kind].loc[i, j] = dists[i][0][j]
+                self.skims[kind].loc[i, j] = paths[i][j]
         
-        # compute diagonal
+        # compute diagonal based on distance
         if diagonal == "density":
             np.fill_diagonal(self.skims[kind].values, \
-            sqrt(self.df_zones["pop"].values / density) * 0.5)
+            np.sqrt(self.df_zones["pop"].values / density) * 0.5)
         else:
             raise NotImplementedError("Only 'density'-based diagonal available.")
             
         # adjust time-related skims
         if kind in ["t0", "tcur"]:
             np.fill_diagonal(self.skims[kind].values, \
-                             self.skims[kind].values.diagonal() / self.v_intra)
-            self.skims[kind] *= 60.0
+                self.skims[kind].values.diagonal() / self.v_intra * 60.0)
               
         
     def compute_skim_utility(self, name, params):
@@ -209,10 +229,10 @@ class MTM():
         Compute OD matrices for a given demand stratum
         via a doubly constrained iterative algorithm
         
-        Input
-        =====
-        - C : cost function as skim matrix, T0, D or other if defined
+        Inputs
+        ======
         - ds : demand stratum
+        - C : cost function as skim matrix, t0, tcur, l or utility
         - func : distribution function, exp or poly
         - param : parameter of the distribution function
         - Nit : number of iterations
@@ -226,16 +246,15 @@ class MTM():
         assert param >= 0.0, "Parameter should be >= 0."
         assert Nit > 0, "Number of iterations should be positive."
         
+        # define set of distribution parameters
         self.dpar.loc[ds] = [C, func, param]
         
         O = self.df_zones[self.dstrat.loc[ds, "prod"]].values.copy()
         D = self.df_zones[self.dstrat.loc[ds, "attr"]].values.copy() * \
-            self.dstrat.loc["all", "coeff"]
+            self.dstrat.loc[ds, "param"]
         O *= D.sum() / O.sum() # normalisation wrt attraction
         
         a, b = np.ones_like(O), np.ones_like(D)
-        errs_O, errs_D, S = [], [], []
-
         T = np.zeros((self.Nz, self.Nz))
         T = np.outer(O, D) * \
             self.dist_func(func, self.skims[C].values, param)
@@ -245,8 +264,9 @@ class MTM():
             T = T * np.outer(a, b)
             b = D / T.sum(0)
 
-            errs_O.append((abs(T.sum(1) - O)).sum() / len(T))
-            errs_D.append((abs(T.sum(0) - D)).sum() / len(T))
+        # compute final mean average errors
+        self.dist_errs = {"O": (abs(T.sum(1) - O)).sum() / len(T) / self.Nz,\
+                         "D": (abs(T.sum(0) - D)).sum() / len(T) / self.Nz}
             
         self.dmats[ds] = pd.DataFrame(T, columns=self.df_zones.index, \
                               index=self.df_zones.index)
@@ -257,56 +277,56 @@ class MTM():
     # =====
     def assign(self, imp, kind="incremental", weights=[50, 50]):
         """
-        Perform final assignment of traffic on the network.
-        Use only one transport system for the start.
+        Perform assignment of traffic to the network.
+        Use only one transport system here.
         
         1. Sum all demand matrices.
-        2. For each weight, calculate all the shortest paths by a criterion
-           (distance or current time) between two zones and add to the links
-           on the path. Update current time.
+        2. For each weight, calculate all the shortest paths 
+           by given impedance (distance or current time) 
+           between two zones and add to the links on the path. 
+           Update current time.
         
         Inputs
         ======
-        - imp : impedance (link attribute) for path search that is computed
-            as a skim matrix
+        - imp : impedance (link attribute) for path search 
+            that is computed as a skim matrix
         - kind : type of assignment
         - weights : assignment weights
         """
         assert kind in self.assignment_kinds, \
-            "Requested assignment kind not available, choose from %s" % \
+            "Assignment kind not available, choose from %s" % \
             self.assignment_kinds
         
         assert imp in self.skims.keys(), \
-            "Impedance %s not found, need to be first computed." % imp
+            "Impedance '%s' not defined." % imp
             
         weights = np.array(weights)
         weights = weights / weights.sum() # normalise weights
         
         # sum all demand matrices into one
-        for i, ds in enumerate(self.dmats.keys()):
-            if i == 0:
-                DM = self.dmats[ds].copy()
-            else:
-                DM += self.dmats[ds]
-        self.dmats["sum"] = DM
+        self.DM = sum(self.dmats.values())
+
+        # remove flows and reset tcur/vcur before assignment
+        nx.set_edge_attributes(self.G, 0.0, "q")
+        self.df_links["q"] = 0.0
         
         if kind == "incremental":
             for wi, w in enumerate(weights):
-                print("Assigning batch %i, weight %.2f ..." % (wi, w))
-                paths = dict(nx.all_pairs_dijkstra(self.G, weight=imp))
+                if self.verbose:
+                    print("Assigning batch %i, weight %.2f ..." % (wi+1, w))
+                    
+                paths = dict(nx.all_pairs_dijkstra_path(self.G, weight=imp))
                 
                 for i in self.df_zones.index:
                     for j in self.df_zones.index:
-                        p = paths[i][1][j]
+                        p = paths[i][j]
                         
-                        # fill the flow column "q" of links dataframe
-                        for ki, _ in enumerate(p[:-1]):
-                            dq = self.dmats["sum"].loc[i, j] * w
-                            self.G.edges[p[ki], p[ki+1]]["q"] += dq
-                            self.df_links.loc[(min(p[ki], p[ki+1]), max(p[ki], p[ki+1])), "q"] += dq
+                        for k, _ in enumerate(p[:-1]):
+                            dq = self.DM.loc[i, j] * w
+                            self.G.edges[p[k], p[k+1]]["q"] += dq
+                            self.df_links.loc[tuple(sorted(p[k:k+2])), "q"] += dq
                             
-                self.compute_tcur_links() # recompute current time
-
+                self.compute_tcur_links() # update current time and speed
 
 
 
