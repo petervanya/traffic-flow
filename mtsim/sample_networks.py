93c995005545c830fedfa05b7a61c56361cd5d3f
petervanya <peter.vanya@gmail.com>
2020-09-03 13:20:10 +0200

    Initial commit

---
+++ mtsim/sample_networks.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+
+
+def load_network_1():
+    """Load a small network of 4 zones and 8 undirected links"""
+    dirn = os.path.dirname(__file__) + "/examples/"
+
+    xls = pd.ExcelFile(dirn + "network_1.xlsx")
+    df_nodes = xls.parse("nodes")
+    df_types = xls.parse("link_types")
+    df_links = xls.parse("links")
+
+    return df_nodes, df_types, df_links
+
+
+def load_network_2():
+    """Load a large network of main corridors of Slovakia"""
+    dirn = os.path.dirname(__file__) + "/examples/"
+
+    xls = pd.ExcelFile(dirn + "network_2.xlsx")
+    df_nodes = xls.parse("nodes")
+    df_types = xls.parse("link_types")
+    df_links = xls.parse("links")
+
+    return df_nodes, df_types, df_links
+
