ef411be229fef7a182578be8d287b4b65291d25d
petervanya <peter.vanya@gmail.com>
2023-08-08 17:52:35 +0200

    testing structure

--- testing/test_optimisation.py
+++ testing/test_optimisation.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
 """
-Optimisation testing
+Testing of the optimisation procedure using Igraph backend.
 
-24/09/20
+2020-09-24
 """
-import os
 import time
-import pandas as pd
-from mtsim import DiMTMig as MTM
-from mtsim.sample_networks import load_network_2_directed
+from mtsim import MTM
+from mtsim.sample_networks import load_network_2
 
-# loading data
-df_nodes, df_link_types, df_links = load_network_2_directed()
+def test_optimise():
+    # loading data
+    df_nodes, df_link_types, df_links = load_network_2()
 
-# MTM run
-mtm = MTM()
-tic = time.time()
-mtm.read_data(df_nodes, df_link_types, df_links)
-mtm.generate("ALL", "pop", "pop", 0.5)
-mtm.compute_skims()
-mtm.distribute("ALL", "tcur", "exp", 0.02)
-#mtm.assign("tcur")
-toc = time.time()
-print("Cycle done.")
-print("Time: %.2f s" % (toc - tic))
+    # MTM run
+    mtm = MTM()
+    tic = time.time()
+    mtm.read_data(df_nodes, df_link_types, df_links)
+    mtm.generate("ALL", "pop", "pop", 0.5)
+    mtm.compute_skims()
+    mtm.distribute("ALL", "tcur", "exp", 0.02)
+    #mtm.assign("tcur")
+    toc = time.time()
+    print("Cycle done.")
+    print("Time: %.2f s" % (toc - tic))
 
-# optimisation
-tic = time.time()
-mtm.optimise(10)
-toc = time.time()
-print(mtm.optres)
+    # optimisation
+    tic = time.time()
+    mtm.optimise(10)
+    toc = time.time()
+    print(mtm.optres)
+
+
+if __name__ == '__main__':
+    test_optimise()
