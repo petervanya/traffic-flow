cc4f8530e99db80d0b8aa19726da64972aa8c0bc
petervanya <peter.vanya@gmail.com>
2023-08-08 17:30:53 +0200

    testing structure

--- testing/test_4_step.py
+++ testing/test_4_step.py
@@ -1,27 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
-Fast testing of the the MTM object.
+A test of the MTM object (macroscopic transport model) with Igraph backend.
 
-03/08/20
+2020-08-03
 """
 import os
 import pandas as pd
-from mtsim import MTMig as MTM
+from mtsim import MTM
 
-# reading data
-xls = pd.ExcelFile(os.path.abspath("../mtsim/examples/network_2.xlsx"))
-df_nodes = xls.parse("nodes")
-df_links = xls.parse("links")
-if "LENGTH" in df_links.columns:
-    df_links = df_links.drop("LENGTH", 1)
-df_link_types = xls.parse("link_types")
 
-# MTM object
-mtm = MTM()
-mtm.read_data(df_nodes, df_link_types, df_links)
-mtm.compute_skims()
-mtm.generate("ALL", "pop", "pop", 0.5)
-mtm.distribute("ALL", "t0", "exp", 0.02)
-mtm.assign("tcur")
+def test_network_2():
+    # reading data
+    xls = pd.ExcelFile(os.path.abspath("mtsim/examples/network_2.xlsx"))
+    df_nodes = xls.parse("nodes")
+    df_links = xls.parse("links")
+    if "LENGTH" in df_links.columns:
+        df_links = df_links.drop("LENGTH", 1)
+    df_link_types = xls.parse("link_types")
 
-print(mtm.df_links)
+    # the model object
+    mtm = MTM()
+    mtm.read_data(df_nodes, df_link_types, df_links)
+    mtm.compute_skims()
+    mtm.generate("ALL", "pop", "pop", 0.5)
+    mtm.distribute("ALL", "t0", "exp", -0.02)
+    mtm.assign("tcur")
+
+    print(mtm['df_links'])
+
+
+if __name__ == '__main__':
+    test_network_2()
