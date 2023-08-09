b47a5133a5d97d30164a7ac896f2100dd4e65a4f
petervanya <peter.vanya@gmail.com>
2023-08-09 10:48:51 +0200

    added multiple funcs

--- testing/test_pipelines.py
+++ testing/test_pipelines.py
@@ -5,42 +5,96 @@ import igraph as ig
 import networkx as nx
 
 import mtsim
-from mtsim import MTM
-from mtsim.sample_networks import load_network_2
 
 print(f'Numpy version: {np.__version__}')
 print(f'Pandas version: {pd.__version__}')
-print(f'igraph version: {ig.__version__}')
-print(f'networkx version: {nx.__version__}')
+print(f'Igraph version: {ig.__version__}')
+print(f'NetworkX version: {nx.__version__}')
 print(f'Quince version: {mtsim.__version__}')
 
-def test_simple_assignment():
+
+def test_network_1_nx():
+    from mtsim import MTMnx
+    from mtsim.sample_networks import load_network_1
+
+    print('\nTesting NetworkX backend, network 1...')
+
+    df_nodes, df_link_types, df_links = load_network_1()
+    
+    model = MTMnx(verbose=True)
+    model.read_data(df_nodes, df_link_types, df_links)
+    
+    model.generate("main-stratum", "pop", "pop", 0.5)
+    model.compute_skims()
+    model.distribute("main-stratum", "tcur", "exp", -0.02)
+    
+    model.assign("tcur")
+
+    print(model.df_links.head())
+
+
+def test_network_2_nx():
+    from mtsim import MTMnx
+    from mtsim.sample_networks import load_network_2
+
+    print('\nTesting NetworkX backend, network 2...')
 
     df_nodes, df_link_types, df_links = load_network_2()
     
-    mtm = MTM(verbose=True)
+    model = MTMnx(verbose=True)
+    model.read_data(df_nodes, df_link_types, df_links)
     
-    # read the road network data
-    mtm.read_data(df_nodes, df_link_types, df_links)
+    model.generate("main-stratum", "pop", "pop", 0.5)
+    model.compute_skims()
+    model.distribute("main-stratum", "tcur", "exp", -0.02)
     
-    # trip generation, create a demand stratum
-    # 'pop' is a zone attribute containing population
-    mtm.generate("main-stratum", "pop", "pop", 0.5)
-    
-    # calculate skim (resistance) matrices for distance and time
-    mtm.compute_skims()
-    
-    # trip distribution of demand stratum 'stratum_1' using the gravity model
-    # distribute the trips exponentially with parameter 0.02
-    # disutility defined as time 'tcur' computed above as a skim matrix
-    mtm.distribute("main-stratum", "tcur", "exp", -0.02)
-    
-    # assign vehicle flows to the network
-    mtm.assign("tcur")
+    model.assign("tcur")
 
-    # print results
-    # print(mtm.)
+    print(model.df_links.head())
+
+
+def test_network_1_ig():
+    from mtsim import MTM
+    from mtsim.sample_networks import load_network_1
+
+    print('\nTesting Igraph backend, network 1...')
+
+    df_nodes, df_link_types, df_links = load_network_1()
+    
+    model = MTM(verbose=True)
+    model.read_data(df_nodes, df_link_types, df_links)
+    
+    model.generate("main-stratum", "pop", "pop", 0.5)
+    model.compute_skims()
+    model.distribute("main-stratum", "tcur", "exp", -0.02)
+    
+    model.assign("tcur")
+
+    print(model.df_links.head())
+
+
+def test_network_2_ig():
+    from mtsim import MTM
+    from mtsim.sample_networks import load_network_2
+    
+    print('\nTesting Igraph backend, network 2...')
+
+    df_nodes, df_link_types, df_links = load_network_2()
+
+    model = MTM(verbose=True)
+    model.read_data(df_nodes, df_link_types, df_links)
+    
+    model.generate("main-stratum", "pop", "pop", 0.5)
+    model.compute_skims()
+    model.distribute("main-stratum", "tcur", "exp", -0.02)
+    
+    model.assign("tcur")
+
+    print(model.df_links.head())
 
 
 if __name__ == '__main__':
-    test_simple_assignment()
+    test_network_1_nx()
+    test_network_2_nx()
+    test_network_1_ig()
+    test_network_2_ig()
