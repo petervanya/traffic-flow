6c06f4ee7d67a0b21be2b9ffea2800bf1b9600d4
petervanya <peter.vanya@gmail.com>
2023-09-26 18:48:41 +0200

    added testing shapefile

--- testing/test_inputs.py
+++ testing/test_inputs.py
@@ -10,12 +10,29 @@ from traffic_flow import MTM
 from traffic_flow.utils import read_inputs_excel, read_inputs_shapefile
 
 def test_reading_csv():
+    print('Testing raw input reading...')
     fname = 'Internal/Networks/network_raw_I51_201002.xlsx'
     df_nodes, df_link_types, df_links = read_inputs_excel(fname)
 
     # first few steps
     model = MTM()
-    print("Backend:", model.backend)
+
+    tic = time.time()
+    model.read_data(df_nodes, df_link_types, df_links)
+    model.generate("ALL", "pop", "pop", 0.5)
+    model.compute_skims()
+    model.distribute("ALL", "tcur", "exp", -0.02)
+    toc = time.time()
+    print("Basic cycle done. Time: %.3f s" % (toc - tic))
+
+def test_reading_shapefile():
+    print('Testing shapefile reading...')
+    basepath = '/Users/peter/Tatra/UHP/Res/Transport_Models/Data/Shapefiles_I51/nulovy_stav/'
+    basename = 'I51_I76_siet_nulovy_stav'
+    df_nodes, df_link_types, df_links = read_inputs_shapefile(basepath, basename)
+
+    # first few steps
+    model = MTM()
 
     tic = time.time()
     model.read_data(df_nodes, df_link_types, df_links)
@@ -26,5 +43,7 @@ def test_reading_csv():
     print("Basic cycle done. Time: %.3f s" % (toc - tic))
 
 
+
 if __name__ == '__main__':
     test_reading_csv()
+    test_reading_shapefile()
